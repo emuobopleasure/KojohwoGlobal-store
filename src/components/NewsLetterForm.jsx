d105cdb179e7c46f981d08e7bc32a90af6ea14d0
Emuobonuvie Pleasure <davis06@outlook.com>
2025-12-30 02:39:10 +0100

    Fix case-sensitive import for useNewsletter hook

--- src/components/NewsLetterForm.jsx
+++ src/components/NewsLetterForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import useNewsletter from '../hooks/useNewsletter';
+import useNewsletter from '../hooks/useNewsLetter.js';
 
 const NewsletterForm = ({ 
   showName = false, 
